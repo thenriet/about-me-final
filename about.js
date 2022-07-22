257ab6b0c2b7d3776125c53a62dfd328a0d5349f
Thomas Henriet <dabalx@gmail.com>
2022-07-22 20:02:47 +0200

    Design update and some animated content

--- about.js
+++ about.js
@@ -1,11 +1,46 @@
-let aboutItems = document.querySelector('.about');
+let aboutItems = document.querySelector('.about-section');
+console.log(aboutItems)
 
-grow = (e) => {
-  e.target.classList.toggle('normal')
-  e.target.classList.toggle('grow')
+visible = (e) => {
+  e.target.classList.toggle('fade-in.visible')
 }
 
-for (var item of aboutItems.children) {
-  item.addEventListener("scroll", grow, false);
-  item.addEventListener("scroll", grow, false);
+// aboutItems.addEventListener("scroll", visible, false);
+
+// for (var item of aboutItems.children) {
+//   item.addEventListener("scroll", grow, false);
+//   item.addEventListener("scroll", grow, false);
+// }
+
+// var options = {
+//   root: document.querySelector('#scrollArea'),
+//   rootMargin: '0px',
+//   threshold: 1.0
+// }
+
+// var observer = new IntersectionObserver(visible, root);
+const targetAbout = document.querySelector('.about-section');
+const targetSkills = document.querySelector('#skills');
+const targetCollaboration = document.querySelector('#collaboration');
+const targetConception = document.querySelector('#conception');
+const targetDev = document.querySelector('#dev');
+const targetProd = document.querySelector('#prod');
+function handleIntersection(entries) {
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.remove('fade-in')
+      entry.target.classList.add('fade-in-visible')
+      entry.target.classList.add('translate')
+    } else {
+      entry.target.classList.remove('fade-in visible')
+    }
+  });
 }
+
+const observer = new IntersectionObserver(handleIntersection);
+observer.observe(targetAbout);
+observer.observe(targetSkills);
+observer.observe(targetCollaboration);
+observer.observe(targetConception);
+observer.observe(targetDev);
+observer.observe(targetProd);
