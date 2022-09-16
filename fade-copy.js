76b028bfe80614113df6fb3a3f2fc812ab3bd0c4
thenriet <dabalx@gmail.com>
2022-09-16 14:00:44 +0200

    fade in and out animations revamped + icons switch

--- fade-copy.js
+++ fade-copy.js
@@ -5,7 +5,7 @@ window.addEventListener('scroll', checkBoxes);
 checkBoxes();
 
 function checkBoxes() {
-  const triggerBottom = window.innerHeight / 5 * 4;
+  const triggerBottom = window.innerHeight / 5 * 3;
 
   boxes.forEach(box => {
     const boxTop = box.getBoundingClientRect().top;
